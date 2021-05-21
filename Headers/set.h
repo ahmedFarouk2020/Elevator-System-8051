a065ce143dae3eaad240a4c490d5af7478f9e5bb
ahmedFarouk2020 <ahmedfarook0111@gmail.com>
2021-05-21 17:28:02 +0200

    update set

--- Headers/set.h
+++ Headers/set.h
@@ -4,15 +4,18 @@
 #define u8 int
 
 u8 requests[10] = {0};
-u8 index; // the place in array where the new value will be placed
-
+u8 queue[5] = {0};
+u8 index = 0;   // the place in array where the new value will be placed
+u8 q_index = 0; // the place in array where the new value will be placed
 /********************************/
 /*          FUNCTIONS            */
 /******************************/
 u8 push_value(u8 value);
 u8 Is_value_exist(u8 value);
 void remove_index(u8 index);
-void display(void);
+u8 enqueue(u8 value);
+u8 dequeue(void);
+void display(u8 arr[]);
 
 
 
