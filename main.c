e41fc4a577006d11ecd51f875d65a5c5cb0308f8
zeyad-khaled <45128084+zeyad-kay@users.noreply.github.com>
2021-05-21 21:41:41 +0200

    put all buttons in the same interrupt

--- main.c
+++ main.c
@@ -2,62 +2,11 @@
 // #include "Headers/stack.h"
 #include "Headers/BIT_MATH.h"
 #include "Headers/STD_TYPES.h"
-// #include "set.h"
-
-// STACK -------------------------------------------
-/* MAX NUM OF ELEMENT IN STACK IS 100 */
-#define MAXSTACK 10
-
-/* REMEBER TO CHANGE THE FN diplay ACCORDING TO THIS */
-#define StackEntry u8
-
-typedef struct
-{
-    u8 top; /* POINTS TO THE LOCATION IN WHICH WE PUT THE DATA (ACCESS THEN INCREAMENT) */
-    StackEntry entry[MAXSTACK];
-
-}Stack;
-void createStack(Stack* ptr_s)
-{
-    ptr_s->top = 0;
-}
-
-u8 push(StackEntry value , Stack* ptr_s)
-{
-    if(ptr_s->top == MAXSTACK)
-        return 0;
-    else    
-    {
-        ptr_s->entry[ptr_s->top++] = value ;
-        return 1;
-    }    
-}
-
-
-u8 StackFull(Stack* ptr_s)
-{
-    return((ptr_s->top == MAXSTACK));
-}
-
-u8 StackEmpty(Stack* ptr_s)
-{
-    return((ptr_s->top == 0));
-}
-
-void pop(StackEntry* pe , Stack* ptr_s)
-{
-    *pe = ptr_s->entry[-- ptr_s->top];
-}
-
-void clearStack(Stack* ptr_s)
-{
-    ptr_s->top = 0;
-}
-//  -------------------------------------------
-
-// SET -----------------------------------------------------
 u8 requests[10] = {0};
-u8 index; // the place in array where the new value will be placed
+u8 queue[5] = {0};
+u8 index = 0;   // the place in array where the new value will be placed
+u8 q_index = 0; // the place in array where the new value will be placed
+u8 q_size = 0;
 u8 push_value(u8 value)
 {
     if(index <= 9){    
@@ -79,6 +28,35 @@ u8 push_value(u8 value)
 
 }
 
+u8 enqueue(u8 value)
+{
+    if(q_size == 0)     q_index = 0;
+    if(q_index <5){
+        queue[q_index] = value;
+        q_index++;
+        q_size++;
+        return 1;
+    }
+    else{ return 0; }
+}
+
+u8 dequeue()
+{
+    u8 counter = 0;
+    u8 poped_data;
+    for(counter; counter <= 5; counter++){
+        if(queue[counter] != 0)
+        {
+            poped_data = queue[counter];
+            queue[counter] = 0;
+            if(counter == 4)    {q_index = 0;}
+            q_size--;
+            return poped_data;
+        }
+    }
+    return 0;
+}
+
 /* 
  * returned Value = 1? value exists
  * returned Value = 0? value isn't exist
@@ -90,7 +68,6 @@ u8 Is_value_exist(u8 value)
     {
         if(value == requests[i])
         {
-            
             return i;
         }
     }
@@ -99,15 +76,6 @@ u8 Is_value_exist(u8 value)
 void remove_index(u8 index)
 {
     requests[index] = 0;
-}
-// -----------------------------------------------------
-void Delay_MS(u16 ms){
-    u16 i;
-    u16 j;
-    for(i=0; i<ms; i++){
-        for(j=0; j<120; j++){
-        }
-    }
 }
 int main()
 {
@@ -123,7 +91,7 @@ int main()
 
     // config cross bar
     XBR0 = 0x00;
-    XBR1 = 0x00;
+    XBR1 = 0x14;
     XBR2 = 0x040; // Cross bar enabled , weak Pull-up enabled
     
     IT0 = 1; // Configure interrupt 0 for falling edge on INT0
@@ -133,15 +101,10 @@ int main()
     P2MDOUT = 0xff;
     P1 = 0x00; //LEDs
     
-    P2 = 0x03; //CONTROL_PORTS
-    // Stack requests;
+    // 01111111
+    P2 = 0x7f; //CONTROL_PORTS
     
-    // createStack(&requests);
-    
-
-    // REQUESTS_PORT = 0x00; //Control
     index = 0;
-    Delay_MS(10);
     while (1)
     {
 
@@ -150,18 +113,51 @@ int main()
 }
 void request(void) interrupt 0
 {
-    push_value(1);
-    if(GET_BIT(P2,1))
+    if(!GET_BIT(P2,0))
     {
-        TOG_BIT(P1,0); //toggle led_2
-        // push 1 (up) to Stack
-        // push(1, &requests);
+        // going down request
+        enqueue(1);
     }
-    if(GET_BIT(P2,0))
+    if(!GET_BIT(P2,1))
     {
-        TOG_BIT(P1,1); //toggle led_2
-        // push 0 (down) to Stack
-        // push(0, &requests);
+        // going up request
+        enqueue(2);
     }
-
+    if(!GET_BIT(P2,2))
+    {
+        // push_value(2);
+        // P0 = requests[0];
+        TOG_BIT(P1, 1);
+        // SET_BIT(P1,2);
+    }
+    if(!GET_BIT(P2,3))
+    {
+        // push_value(3);
+        // P0 = requests[0];
+        TOG_BIT(P1, 1);
+        // SET_BIT(P1,3);
+    }
+    if(!GET_BIT(P2,4))
+    {
+        // push_value(0);
+        // P0 = requests[0];
+        TOG_BIT(P1, 1);
+        // SET_BIT(P1,4);
+    }
+    if(!GET_BIT(P2,5))
+    {
+        // push_value(0);
+        // P0 = requests[0];
+        TOG_BIT(P1, 1);
+        // SET_BIT(P1,5);
+    }
+    if(!GET_BIT(P2,6))
+    {
+        // push_value(0);
+        // P0 = requests[0];
+        TOG_BIT(P1, 1);
+        // SET_BIT(P1,6);
+    }
+    
+    
 }
